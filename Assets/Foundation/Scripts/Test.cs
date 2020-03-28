af8eb2a0a6d6b333921d6edaa52aae08ec65e30b
wolfi <wolfi@bytenine.io>
2020-03-28 22:45:30 +0900

    test: Set resolution list

---
+++ Assets/Foundation/Scripts/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var res in Screen.resolutions)
+        {
+            Debug.Log($"{res.width} , {res.height}");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
