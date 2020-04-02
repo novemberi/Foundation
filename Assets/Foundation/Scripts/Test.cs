2b9fb92a1abb7cedb365d1d7f45411942754b7f3
wolfi <wolfi@bytenine.io>
2020-04-02 19:20:38 +0900

    feat: Dropdown resolution list

--- Assets/Foundation/Scripts/Test.cs
+++ Assets/Foundation/Scripts/Test.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.UI;
 
 public class Test : MonoBehaviour
 {
+    [SerializeField]
+    private Dropdown dropDown;
+
     // Start is called before the first frame update
     void Start()
     {
-        foreach (var res in Screen.resolutions)
-        {
-            Debug.Log($"{res.width} , {res.height}");
-        }
+        dropDown.options = Screen.resolutions.Select(r => new Dropdown.OptionData($"{r.width} * {r.height}")).ToList();
     }
 
     // Update is called once per frame
